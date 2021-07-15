0187bf3671e3a5f28c43eb739c726396a6e3a80b
Matteo Lorenzo <72198994+Matteo-Lorenzo@users.noreply.github.com>
2021-07-15 16:35:12 +0200

    Update settings.py

--- pyRisProject/settings.py
+++ pyRisProject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pyrisdb',
-        'USERNAME': 'postgres',
+        'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
